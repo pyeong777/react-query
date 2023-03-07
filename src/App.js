b246a3fe6fd853c90da3731ea18945952e923837
pyeong777 <sdp1123@naver.com>
2023-03-07 18:27:15 +0900

    Add: staleTime

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
 import MainProducts from "./components/MainProducts";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +10,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
